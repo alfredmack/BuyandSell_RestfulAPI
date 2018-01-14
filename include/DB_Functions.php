809e023beb581f7b1966ba73ad1d4355a65a246d
Alfred Mack <alfredmack@gmail.com>
2018-01-14 19:35:59 +0300

    bug fixed in register method

--- include/DB_Functions.php
+++ include/DB_Functions.php
@@ -83,56 +83,26 @@ class DB_Functions {
     }
     
     
-     /**
-     * Search for username & password
-     */
-//    public function getListofHouses() {
-//         
-//        $result = mysqli_query($this->conn,"SELECT *FROM items_db WHERE category = 'houses'");
-//    
-//       if (mysqli_num_rows($result) > 0) {
-//        
-//            $result = mysqli_fetch_array($result);
-//            
-//            $response["houses"] = array();
-// 
-//    while ($row = mysql_fetch_array($result)) {
-//        // temp user array
-//        $product = array();
-// 
-//        $product["landlord_mail"] = $row["tenant_email"];
-//        $product["fullname"] = $row["fullname"];
-//        
-// 
-//        // push single product into final response array
-//        array_push($response["houses"], $product);
-//    }
-//    // success
-//    $response["success"] = 1;
-// 
-//    // echoing JSON response
-//    echo json_encode($response);
-//       
-//            // success
-//            $response["success"] = 1;
-//            $response["message"] = "Success Login";
-//
-//            // echoing JSON response
-//            return $response;
-//        }
-//        else{
-//            
-//            $response["success"] = 0;
-//            $response["message"] = "Usename or Password is incorect";
-//
-//            // echoing JSON response
-//            return $response;
-//            
-//        }
-//        
-//      
-//    }
-    
+  public function regUsers($fname,$lname,$email,$gender,$username, $password) {
+          
+         
+        $result = mysqli_query($this->conn,"INSERT INTO login_tb(username,password) VALUES('$username', '$password')");
+         
+     if (!mysql_error()) {
+         
+         mysqli_query($this->conn,"INSERT INTO users_tb(fname,lname,email,gender,username) VALUES('$fname', '$lname','$email','$gender','$username')");
+         $response["success"] = 1;
+         $response["message"] = "User successfully created.";
+         return $response;
+     }
+     else{
+         $response["success"] = 0;
+         $response["message"] = "User failed to be created.";
+         return $response;
+     }
+             
+         }
+         
     
         
 }
